698d6990894fb5f919b12e062deb469b4457c05a
Daniel Beadle <djbeadle@gmail.com>
2022-04-27 12:18:39 -0400

    Add waveforms!!

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -2,16 +2,11 @@ function showAudioPlayer(filename) {
     document.getElementById(filename).classList.toggle("audio-player");
 }
 
-function changeTrack(filename) {
-    if (`https://audio-manager.s3.amazonaws.com/${filename}` !== document.getElementById("static-audio-player").src) {
-        document.getElementById("static-audio-player").src=`https://audio-manager.s3.amazonaws.com/${filename}`;
-    }
+function changeTrack(filename, wavesurfer) {
+    wavesurfer.load(`https://audio-manager.s3.amazonaws.com/${filename}`);
 
     let player = document.getElementById("static-audio-player");
 
-    if (player.paused) {
-        player.play();
-    } else {
-        player.pause();
-    }
+    wavesurfer.play();
+
 }
