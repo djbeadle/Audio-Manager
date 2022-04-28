6bc0f17a77f182d3aabd59b2172884ea8cd67065
djbeadle <djbeadle@gmail.com>
2022-04-28 01:50:14 +0000

    Prevent audio preloading and Wavesurfer from double loading

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -3,10 +3,9 @@ function showAudioPlayer(filename) {
 }
 
 function changeTrack(filename, wavesurfer) {
+    wavesurfer.pause();
     wavesurfer.load(`https://audio-manager.s3.amazonaws.com/${filename}`);
 
     let player = document.getElementById("static-audio-player");
-
-    wavesurfer.play();
-
 }
+
